9b682b2675cb16892b13a561571983b86becb1a5
Rita Klebesz <r.klebesz@synoa.de>
2017-09-20 11:37:22 +0200

    BETT-391: add default logger, increase version number

---
+++ src/main/resources/archetype-resources/src/main/java/configurations/DefaultLogger.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.configurations;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultLogger {
+
+    @Bean
+    public Logger createDefaultLogger() {
+        return LoggerFactory.getLogger("${groupId}.${artifactId}");
+    }
+
+}
