270ee7e66673580ebfe43f4068a6f6062d5249f9
Oliver Koch <o.koch@synoa.de>
2018-01-22 15:14:08 +0100

    AP-21: added Mongobee for Schema Migration to the Archetype
    fixes #23

--- src/main/resources/archetype-resources/src/main/java/configurations/MongoDB.java
+++ src/main/resources/archetype-resources/src/main/java/configurations/MongoDB.java
@@ -3,15 +3,15 @@
 #set( $symbol_escape = '\' )
 package ${package}.configurations;
 
-import java.util.Collections;
-
+import com.github.mongobee.Mongobee;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
+import java.util.Collections;
 
 @Configuration
 public class MongoDB {
@@ -30,4 +30,20 @@ public class MongoDB {
         return new MongoClient(address, Collections.singletonList(credential));
     }
 
+    @Bean
+    public Mongobee mongobee(@Value("${mongodb.host}") String host, @Value("${mongodb.port}") int port,
+            @Value("${mongodb.user}") String user, @Value("${mongodb.database}") String database,
+            @Value("${mongodb.password}") String password) {
+
+        Mongobee runner = new Mongobee(createMongoClient(host, port, user, database, password));
+        runner.setDbName(database);
+        // the package to be scanned for changesets
+        runner.setChangeLogsScanPackage("${package}.changelogs");
+        // collection with applied change sets
+        runner.setChangelogCollectionName("${artifactId}.changelog");
+        // collection used during migration process
+        runner.setLockCollectionName("${artifactId}.lock");
+
+        return runner;
+    }
 }
